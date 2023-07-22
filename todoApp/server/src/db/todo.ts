202ad59b1a1650cdf20a9af14691442a73e46edf
vladistr08 <vladistra08@gmail.com>
2023-07-22 17:18:31 +0300

    add schema full and resolver files + define crud operations

---
+++ todoApp/server/src/db/todo.ts
@@ -0,0 +1,54 @@
+import {ITodo, Todo} from '../models/todo'
+
+export const getAllTodos = async (userId: string): Promise<ITodo[]> => {
+    try {
+        return await Todo.find({userId: userId}).exec()
+    }catch (e) {
+        throw new Error(`Error getting all todos ${e.message}`)
+    }
+}
+
+export const updateTodo = async (todoId: string, props: Partial<ITodo>): Promise<ITodo> => {
+    try {
+        const result = await Todo.findOneAndUpdate({todoId: todoId}, {...props})
+        return result[0]
+    }catch (e) {
+        throw new Error(`Error updating todos ${e.message}`)
+    }
+}
+
+export const addTodo = async (todo: ITodo): Promise<string> => {
+    try {
+        const {todoId} = await Todo.create(todo)
+        return todoId.toString()
+    }catch (e) {
+        throw new Error(`Error creating todo ${e.message}`)
+    }
+}
+
+export const deleteTodo = async (todoId: string): Promise<boolean> => {
+    try{
+        const {deletedCount} = await Todo.deleteOne({todoId: todoId})
+        return deletedCount > 0
+    }catch (e) {
+        throw new Error(`Error deleting todo ${e.message}`)
+    }
+}
+
+export const getTodoIdByName = async (userId: string, todoName: string): Promise<string> => {
+    try {
+        const result = await Todo.findOne({userId: userId, name: todoName})
+        return result[0].todoId
+    }catch (e) {
+        throw new Error(`Error getting todo by name ${e.message}`)
+    }
+}
+
+export const getTodoById = async (todoId: string): Promise<ITodo> => {
+    try{
+        const result = await Todo.findOne({todoId: todoId})
+        return result[0]
+    }catch (e) {
+        throw new Error(`Error getting todo by id ${e.message}`)
+    }
+}
